8ee4fdaffcefb31cda3c265a0b8e3aa08ee1dc9b
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-09 21:32:39 +0430

    init

---
+++ githubfa.py
@@ -0,0 +1,37 @@
+import tweepy
+import json
+
+# Authentication details. To  obtain these visit dev.twitter.com
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# This is the listener, resposible for receiving data
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        tweet = json.loads(data)
+
+        api.retweet(tweet['id'])
+        print('retweeted' + str(tweet['id']))
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    #print("Showing all new persian tweets for github:")
+
+    # There are different kinds of streams: public stream, user stream, multi-user streams
+    # In this example follow #programming tag
+    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    l = StdOutListener()
+    stream = tweepy.Stream(auth, l)
+    stream.filter(track=['github com'], languages=['fa'])
