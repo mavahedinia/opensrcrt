9124feb2aaabf2b1e520a1ef5e26b54d8903c9e1
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-09 22:43:11 +0430

    some exception handling :)

--- githubfa.py
+++ githubfa.py
@@ -17,8 +17,11 @@ class StdOutListener(tweepy.StreamListener):
         # Twitter returns data in JSON format - we need to decode it first
         tweet = json.loads(data)
 
-        api.retweet(tweet['id'])
-        print('retweeted' + str(tweet['id']))
+        try:
+            api.retweet(tweet['id'])
+            print(tweet['id_str'] + ' rewteeted!')
+        except:
+            print('failed to retweet ' + tweet['id_str'])
         return True
 
     def on_error(self, status):
@@ -34,4 +37,4 @@ if __name__ == '__main__':
     # For more details refer to https://dev.twitter.com/docs/streaming-apis
     l = StdOutListener()
     stream = tweepy.Stream(auth, l)
-    stream.filter(track=['github com'], languages=['fa'])
+    stream.filter(track=['github com', 'gitlab com'], languages=['fa'])
